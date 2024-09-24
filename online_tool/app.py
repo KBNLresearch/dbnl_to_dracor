dc9cfa8057ba7f34453647513157c6cb1dfcdc14
WillemJanFaber <willemjan.faber@kb.nl>
2024-09-24 15:18:15 +0200

    Added batch and don't forget URL fix

--- online_tool/app.py
+++ online_tool/app.py
@@ -1,33 +1,39 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request
+import requests
+import pprint
+
+from flask import (Flask,
+                  jsonify,
+                  render_template,
+                  request,
+                  Response)
+
+from lxml import etree
 from markupsafe import escape
 from urllib.parse import urlparse
-from lxml import etree
-import requests
-
 
 app = Flask(__name__)
 
-#https://www.dbnl.org/nieuws/xml.php?id=vond001gysb01
-#https://www.dbnl.org/nieuws/xml.php?id=vond001gysb01
-
-'''
-<pb> elementen inclusief attributen ('<pb.*?>')
-<hi> elementen inclusief attributen, exclusief inhoud ('<hi.*?>' + '<hi.*?\n.*?>' + '</hi>')
-"rend" attributen inclusief waardes (' rend=".*?"')
-<xptr> elementen inclusief attributen ('<xptr.*?>')
-<note> elementen inclusief attributen en inclusief inhoud ('<note.*?</note>' + '<note.*?\n.*?</note>' + '<note.*?\n.*?\n.*?</note>' + '<note.*?\n.*?\n.*?\n.*?</note>' (in die volgorde))
-'''
-import pprint
+default_url = 'https://www.dbnl.org/nieuws/xml.php?id=vond001gysb01'
 
 default_params = {'pb' : False,
                   'hi' : False,
                   'rend' : False,
-                  'xptr' : False,   
-                  'note': False}
+                  'xptr' : False, 
+                  'note': False,
+                  'url' : default_url}
 
-default_url = 'https://www.dbnl.org/nieuws/xml.php?id=vond001gysb01'
+
+def extract_playlist(url=default_url):
+    req = requests.get(url)
+    parse = etree.XMLParser(remove_blank_text=True)
+    data = req.content
+    xml = etree.fromstring(data,parser=parse)
+
+    for item in xml.iter():
+        print(item.attrib, item.tag, item.text)
+
 
 def parse_xml(url=default_url, params=default_params):
     req = requests.get(url)
@@ -37,7 +43,7 @@ def parse_xml(url=default_url, params=default_params):
     formatted_xml = etree.tostring(xml,
                                    pretty_print = True,
                                    encoding = 'utf-8').decode()
-    stats = len(formatted_xml)
+    start_len = len(formatted_xml)
     if params.get('pb'):
         '''
         <pb> elementen inclusief attributen ('<pb.*?>')'''
@@ -96,8 +102,38 @@ def parse_xml(url=default_url, params=default_params):
     formatted_xml = etree.tostring(xml,
                                    pretty_print = True,
                                    encoding = 'utf-8').decode()
-    stats1 = len(formatted_xml)
-    return formatted_xml, stats - stats1
+    end_len = len(formatted_xml)
+    return formatted_xml, start_len - end_len
+
+
+@app.route("/batch", methods=['GET'])
+def batch():
+    ''' 
+        For batch handling use this, default operation == remove.
+
+        /batch/?todo=groo001adam01 == /batch/?operation=remove&todo=groo001adam01
+        /batch/?operation=playerslist&todo=groo001adam01
+
+        We will handle one xml at the time, so the response is one xml.
+    '''
+
+    opdict = default_params
+    for i in opdict:
+        if type(opdict[i]) == bool:
+            opdict[i] = not opdict[i]
+    todo = request.args.get('todo') or ''
+    operation = request.args.get('operation') or 'remove'
+
+    if not todo:
+        return 'Missing argument todo, /batch/?todo=heyn003vrie01'
+
+    if operation == 'remove': 
+        to_parse = f'https://www.dbnl.org/nieuws/xml.php?id={todo}'
+        xml_data, stats = parse_xml(to_parse, opdict)
+        return Response(xml_data, mimetype='text/xml')
+
+    return 'You should not be seeing this..'
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -128,6 +164,7 @@ def index():
         to_parse = f'https://www.dbnl.org/nieuws/xml.php?id={xml_id}'
         xml_data, stats = parse_xml(to_parse, opdict)
         opdict['stats'] = stats
+        opdict['url'] = to_parse
         return render_template('index.html',
                                xml_data=xml_data,
                                opdict=opdict)
