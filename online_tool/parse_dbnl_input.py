2dad44fb5092fb330dba1754bf47d49cb6a89fcd
Willem Jan Faber <WillemJan@users.noreply.github.com>
2024-09-25 15:44:14 +0200

    Update parse_dbnl_input.py
    Cleanup unused code.

--- online_tool/parse_dbnl_input.py
+++ online_tool/parse_dbnl_input.py
@@ -7,34 +7,13 @@ import datetime
 from pprint import pprint
 
 import lxml.html
-import pandas as pd
 
 from dracor_utils import escape, unescape
 
-
-
 # DBN download TEI-files.
 BASEURL = f'https://www.dbnl.org/nieuws/xml.php?id=%s'
-DBNL_AANLEVER = 'Bibliografische metadata RiR gepubliceerd in 1500-1700.xlsx'
-LUCAS_AANLEVER = 'Inventarisatie_toneelstukken_DBNL.xlsx'
-
-DBNL_DIR = 'dbnl_xml'
-DRACOR_DIR = 'dracor_xml'
-
-LEVENSTEIN_SPEAKER = 3
-
-OUTDIR = 'playlist_per_work'
 
 import datetime
-
-print("Running parse_dbnl_input.py on %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-
-if not os.path.isdir(DBNL_DIR):
-    os.mkdir(DBNL_DIR)
-
-if not os.path.isdir(DRACOR_DIR):
-    os.mkdir(DRACOR_DIR)
-
 
 def print_dracor_xml(data: dict) -> None:
     #pprint(data)
@@ -45,79 +24,6 @@ def print_dracor_xml(data: dict) -> None:
 
     with open(fname, 'w') as fh:
         fh.write(generated_xml)
-
-
-def parse_dbnl_aanlever() -> list[dict] | None:
-    if not os.path.isfile(DBNL_AANLEVER):
-        print(f"Could not read '%s', missing file?" % DBNL_AANLEVER)
-        return None
-    try:
-        df = pd.read_excel(DBNL_AANLEVER)
-        wanted = json.loads(df.to_json())
-    except:
-        print(f"Error parsing %s, file corrupt?" % DBNL_AANLEVER)
-        return None
-
-    all_data = []
-    for nr in wanted.get('ti_id').keys():
-        data = {}
-        fn = 'dbnl_xml' + os.sep + str(wanted.get('ti_id').get(nr)) + '.xml'
-        if not os.path.isfile(fn):
-            # Fetch input xml from DBNL
-            res = requests.get(BASEURL % wanted.get('ti_id').get(nr))
-            if not res.status_code == 200:
-                print(f"Error getting %s" % fn)
-            with open(fn, 'w') as fh:
-                fh.write(res.content.decode('utf-8'))
-
-        for key in wanted.keys():
-            value = wanted.get(key).get(nr)
-            if value is None:
-                continue
-
-            if not key in data:
-                data[key] = value
-
-        all_data.append(data)
-    return all_data
-
-
-def parse_lucas_aanlever(data: list[dict]) -> dict | None:
-    if not os.path.isfile(LUCAS_AANLEVER):
-        print(f"Could not read '%s', missing file?" % LUCAS_AANLEVER)
-        return None
-
-    try:
-        df = pd.read_excel(LUCAS_AANLEVER)
-        wanted = json.loads(df.to_json())
-    except:
-        print(f"Error parsing %s, file corrupt?" % LUCAS_AANLEVER)
-        return None
-
-    all_rows = list(wanted.keys())
-
-    eratta = {}
-
-    for nr in wanted.get('dbnl-ID').keys():
-
-        if not wanted.get('dbnl-ID').get(nr):
-            continue
-
-        cur_id = wanted.get('dbnl-ID').get(nr)
-        if cur_id in [i.get('ti_id') for i in data]:
-            eratta[cur_id] = {}
-            for k in all_rows:
-                if wanted.get(k).get(nr):
-                    eratta[cur_id][k] = wanted.get(k).get(nr)
-        else:
-            print(f"{cur_id} not found in {DBNL_AANLEVER}")
-
-    for item in data:
-        if not item.get('ti_id') in eratta:
-            print(f"{item.get('ti_id')} not in {LUCAS_AANLEVER}")
-
-    return eratta
-
 
 def speaker_filter(speakerlist: set, newspeaker: str) -> set | tuple:
     newspeaker = escape(newspeaker)
