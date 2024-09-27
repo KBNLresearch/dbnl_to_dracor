fc41700380c09c2f0b7cce376b6845d0d433df27
WillemJanFaber <willemjan.faber@kb.nl>
2024-09-27 12:13:07 +0200

    more simpel

--- online_tool/app.py
+++ online_tool/app.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python3
 
+from urllib.parse import urlparse
+
 import requests
-import pprint
+from flask import Flask, Response, jsonify, render_template, request
+from lxml import etree
+from markupsafe import escape
 
 from parse_dbnl_input import parse_fulltext
 
-from flask import (Flask,
-                  jsonify,
-                  render_template,
-                  request,
-                  Response)
-
-from lxml import etree
-from markupsafe import escape
-from urllib.parse import urlparse
-
 app = Flask(__name__)
 
-DEFAULT_URL = 'https://www.dbnl.org/nieuws/xml.php?id=vond001gysb01'
+DEFAULT_URL = "https://www.dbnl.org/nieuws/xml.php?id=vond001gysb01"
 
-DEFAULT_PARAMS = {'remove_pb' : False,
-                  'remove_hi' : False,
-                  'remove_note': False,
-                  'remove_rend' : False,
-                  'remove_xptr' : False,
-                  'stats': 0, 
-                  'mode' : 'remove',
-                  'url' : DEFAULT_URL}
+DEFAULT_PARAMS = {
+    "remove_pb": False,
+    "remove_hi": False,
+    "remove_note": False,
+    "remove_rend": False,
+    "remove_xptr": False,
+    "stats": 0,
+    "mode": "remove",
+    "url": DEFAULT_URL,
+}
 
 
 def fetch_xmldata(url) -> etree:
@@ -45,88 +41,85 @@ def extract_speakerlist(xml):
     out += "</xml>"
     return out
 
+
 def parse_xml(xml, params):
-    formatted_xml = etree.tostring(xml,
-                                   pretty_print = True,
-                                   encoding = 'utf-8').decode()
+    formatted_xml = etree.tostring(
+        xml, pretty_print=True, encoding="utf-8").decode()
     start_len = len(formatted_xml)
-    if params.get('remove_pb'):
-        '''
-        <pb> elementen inclusief attributen ('<pb.*?>')'''
+    if params.get("remove_pb"):
+        """
+        <pb> elementen inclusief attributen ('<pb.*?>')"""
         to_remove = set()
         for i in xml.iter():
-            if str(i.tag).startswith('pb'):
+            if str(i.tag).startswith("pb"):
                 to_remove.add(str(i.tag))
 
         for elm in to_remove:
             etree.strip_tags(xml, elm)
 
-    if params.get('remove_hi'):
-        '''
+    if params.get("remove_hi"):
+        """
         <hi> elementen inclusief attributen,
-        exclusief inhoud ('<hi.*?>' + '<hi.*?\n.*?>' + '</hi>')'''
+        exclusief inhoud ('<hi.*?>' + '<hi.*?\n.*?>' + '</hi>')"""
 
         to_remove = set()
         for i in xml.iter():
-            if str(i.tag).startswith('hi'):
+            if str(i.tag).startswith("hi"):
                 to_remove.add(str(i.tag))
 
         for elm in to_remove:
             etree.strip_tags(xml, elm)
 
-    if params.get('remove_rend'):
-        '''
-        "rend" attributen inclusief waardes (' rend=".*?"')'''
+    if params.get("remove_rend"):
+        """
+        "rend" attributen inclusief waardes (' rend=".*?"')"""
         for elem in xml.iter():
-            if 'rend' in elem.attrib:
-                del elem.attrib['rend']
+            if "rend" in elem.attrib:
+                del elem.attrib["rend"]
 
-    if params.get('remove_xptr'):
-        ''' 
-        <xptr> elementen inclusief attributen ('<xptr.*?>')'''
+    if params.get("remove_xptr"):
+        """
+        <xptr> elementen inclusief attributen ('<xptr.*?>')"""
         to_remove = set()
         for i in xml.iter():
-            if str(i.tag).startswith('xptr'):
+            if str(i.tag).startswith("xptr"):
                 to_remove.add(str(i.tag))
 
         for elm in to_remove:
             etree.strip_tags(xml, elm)
 
-
-    if params.get('remove_note'):
+    if params.get("remove_note"):
         to_remove = set()
         for i in xml.iter():
-            if str(i.tag).startswith('note'):
+            if str(i.tag).startswith("note"):
                 to_remove.add(str(i.tag))
 
         for elm in to_remove:
             etree.strip_tags(xml, elm)
 
         for elem in xml.iter():
-            if 'note' in elem.attrib:
-                del elem.attrib['note']
+            if "note" in elem.attrib:
+                del elem.attrib["note"]
 
-
-
-    formatted_xml = etree.tostring(xml,
-                                   pretty_print = True,
-                                   encoding = 'utf-8').decode()
+    formatted_xml = etree.tostring(
+        xml, pretty_print=True, encoding="utf-8").decode()
 
     end_len = len(formatted_xml)
 
     return formatted_xml, start_len - end_len
 
-@app.route("/extract", methods=['POST'])
-@app.route("/extract/", methods=['POST'])
-@app.route("/", methods=['GET', 'POST'])
+
+@app.route("/extract", methods=["POST"])
+@app.route("/extract/", methods=["POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     operation_params = DEFAULT_PARAMS.copy()
-    if request.method == 'GET':
-        return render_template('index.html', opdict=operation_params)
+    if request.method == "GET":
+        return render_template("index.html", opdict=operation_params)
 
-        operation_params['stats'] = stats
-    todo = request.form.get('url')
-    operation = request.form.getlist('op')
+        operation_params["stats"] = stats
+    todo = request.form.get("url")
+    operation = request.form.getlist("op")
 
     for op in operation:
         operation_params[op] = True
@@ -134,42 +127,41 @@ def index():
     url = urlparse(todo)
 
     if url.hostname is None:
-        return render_template('index.html', opdict=operation_params)
+        return render_template("index.html", opdict=operation_params)
 
-    if not '=' in url.query:
-        return render_template('index.html', opdict=operation_params)
+    if not "=" in url.query:
+        return render_template("index.html", opdict=operation_params)
 
     try:
-        xml_id = escape(url.query.split('=')[-1])
+        xml_id = escape(url.query.split("=")[-1])
     except:
-        return render_template('index.html', opdict=operation_params)
+        return render_template("index.html", opdict=operation_params)
 
-    dbnl_url = f'https://www.dbnl.org/nieuws/xml.php?id={xml_id}'
-    operation_params['url'] = dbnl_url
+    dbnl_url = f"https://www.dbnl.org/nieuws/xml.php?id={xml_id}"
+    operation_params["url"] = dbnl_url
 
-    if not 'extract' in request.path:
+    if not "extract" in request.path:
         xml_data, stats = parse_xml(fetch_xmldata(dbnl_url), operation_params)
-        operation_params['stats'] = stats
-        operation_params['mode'] = 'remove'
+        operation_params["stats"] = stats
+        operation_params["mode"] = "remove"
     else:
         xml_data = extract_speakerlist(fetch_xmldata(dbnl_url))
-        operation_params['mode'] = 'extract'
+        operation_params["mode"] = "extract"
 
-    return render_template('index.html',
-                           xml_data=xml_data,
-                           opdict=operation_params)
+    return render_template("index.html", xml_data=xml_data, opdict=operation_params)
 
-@app.route("/batch", methods=['GET'])
-@app.route("/batch/", methods=['GET'])
+
+@app.route("/batch", methods=["GET"])
+@app.route("/batch/", methods=["GET"])
 def batch_operation() -> Response:
-    ''' 
-        For batch handling use this, default operation == remove.
+    """
+    For batch handling use this, default operation == remove.
 
-        /batch/?todo=groo001adam01 == /batch/?operation=remove&todo=groo001adam01
-        /batch/?operation=playerslist&todo=groo001adam01
+    /batch/?todo=groo001adam01 == /batch/?operation=remove&todo=groo001adam01
+    /batch/?operation=playerslist&todo=groo001adam01
 
-        We will handle one xml at the time, so the response is one xml.
-    '''
+    We will handle one xml at the time, so the response is one xml.
+    """
 
     operation_params = DEFAULT_PARAMS.copy()
 
@@ -177,18 +169,18 @@ def batch_operation() -> Response:
         if isinstance(operation_params[key], bool):
             operation_params[key] = not operation_params[key]
 
-    todo = request.args.get('todo')
+    todo = request.args.get("todo")
     if not todo:
         return "Missing 'todo' argument. Use /batch/?todo=heyn003vrie01", 400
 
-    operation = request.args.get('operation') or 'remove'
-    if operation == 'remove': 
-        to_parse = f'https://www.dbnl.org/nieuws/xml.php?id={todo}'
+    operation = request.args.get("operation") or "remove"
+    if operation == "remove":
+        to_parse = f"https://www.dbnl.org/nieuws/xml.php?id={todo}"
         xml_data, stats = parse_xml(fetch_xmldata(to_parse), operation_params)
-        return Response(xml_data, mimetype='text/xml')
+        return Response(xml_data, mimetype="text/xml")
 
     return "Unknown operation error.", 500
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- online_tool/parse_dbnl_input.py
+++ online_tool/parse_dbnl_input.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python3
 
-import json
-import os
-import datetime
 
-from pprint import pprint
+def escape(str_xml: str):
+    str_xml = str_xml.replace("&", "&amp;")
+    str_xml = str_xml.replace("<", "&lt;")
+    str_xml = str_xml.replace(">", "&gt;")
+    str_xml = str_xml.replace('"', "&quot;")
+    str_xml = str_xml.replace("'", "&apos;")
+    return str_xml
 
-import lxml.html
 
-from dracor_utils import escape, unescape
+def unescape(str_xml: str):
+    str_xml = str_xml.replace("&amp;", "&")
+    str_xml = str_xml.replace("&lt;", "<")
+    str_xml = str_xml.replace("&gt;", ">")
+    str_xml = str_xml.replace("&quot;", '"')
+    str_xml = str_xml.replace("&apos;", "'")
+    return str_xml
 
-# DBN download TEI-files.
-BASEURL = f'https://www.dbnl.org/nieuws/xml.php?id=%s'
-
-import datetime
-
-def print_dracor_xml(data: dict) -> None:
-    #pprint(data)
-    generated_xml = Template(xml_template).render(data=data)
-
-    # Output directory.
-    fname = os.path.join(DRACOR_DIR, data.get('ti_id') + '_dracor.xml')
-
-    with open(fname, 'w') as fh:
-        fh.write(generated_xml)
 
 def speaker_filter(speakerlist: set, newspeaker: str) -> set | tuple:
     newspeaker = escape(newspeaker)
@@ -48,16 +42,14 @@ def parse_fulltext(data):
     read_order = []
 
     alias = {}
-    ctype = ''
+    ctype = ""
     nexupspeaker = False
     srec1 = False
 
-
     for item in data.iter():
-
-        if item.attrib.get('rend', '') == 'speaker' and item.text:
+        if item.attrib.get("rend", "") == "speaker" and item.text:
             target = item.text.strip()
-            if target.endswith('.'):
+            if target.endswith("."):
                 target = target[:-1]
             if not escape(target) in speakerlist:
                 speakerinfo = speaker_filter(speakerlist, target)
@@ -65,16 +57,16 @@ def parse_fulltext(data):
                     alias[speakerinfo[0]] = speakerinfo[1]
                 else:
                     speakerlist = speakerinfo
-        if item.attrib.get('rend', '') == 'speaker':
+        if item.attrib.get("rend", "") == "speaker":
             srec = True
 
-        if item.tag == 'speaker':
+        if item.tag == "speaker":
             srec1 = True
 
-        if srec1 and item.tag == 'hi':
+        if srec1 and item.tag == "hi":
             srec1 = False
             target = item.text.strip()
-            if target.endswith('.'):
+            if target.endswith("."):
                 target = target[:-1]
             if not escape(target) in speakerlist:
                 speakerinfo = speaker_filter(speakerlist, target)
@@ -82,14 +74,12 @@ def parse_fulltext(data):
                     alias[speakerinfo[0]] = speakerinfo[1]
                 else:
                     speakerlist = speakerinfo
-
-
 
         if srec and item.text:
             srec = False
             if not escape(item.text.strip()) in speakerlist:
                 target = item.text.strip()
-                if target.endswith('.'):
+                if target.endswith("."):
                     target = target[:-1]
                 speakerinfo = speaker_filter(speakerlist, target)
                 if type(speakerinfo) == tuple:
@@ -97,50 +87,52 @@ def parse_fulltext(data):
                 else:
                     speakerlist = speakerinfo
 
-
-        if item.tag == 'div':
-            if item.attrib.get('type') == 'act':
+        if item.tag == "div":
+            if item.attrib.get("type") == "act":
                 rec = True
                 if acts:
-                    read_order.append({'act': acts})
-                acts = ['']
-                ctype = 'act'
-            if item.attrib.get('type') == 'chapter':
+                    read_order.append({"act": acts})
+                acts = [""]
+                ctype = "act"
+            if item.attrib.get("type") == "chapter":
                 rec = True
                 if chapters:
-                    read_order.append({'chapter': chapters})
-                chapters = ['']
-                ctype =  'chapter'
-            if item.attrib.get('type') == 'play':
+                    read_order.append({"chapter": chapters})
+                chapters = [""]
+                ctype = "chapter"
+            if item.attrib.get("type") == "play":
                 rec = True
                 if plays:
-                    read_order.append({'play': plays})
-                ctype = 'play'
-                plays = ['']
+                    read_order.append({"play": plays})
+                ctype = "play"
+                plays = [""]
 
         if rec:
-            if item.text and item.attrib.get('rend', '') == 'speaker' and item.text.strip():
-                speak_xml = '\n<speaker>' + escape(item.text) + '</speaker>\n'
-                if ctype == 'chapter':
-                   chapters[-1] += speak_xml
-                if ctype == 'act':
-                   acts[-1] += speak_xml
-                if ctype == 'play':
-                   plays[-1] += speak_xml
-            elif item.attrib.get('rend', '') == 'speaker':
+            if (
+                item.text
+                and item.attrib.get("rend", "") == "speaker"
+                and item.text.strip()
+            ):
+                speak_xml = "\n<speaker>" + escape(item.text) + "</speaker>\n"
+                if ctype == "chapter":
+                    chapters[-1] += speak_xml
+                if ctype == "act":
+                    acts[-1] += speak_xml
+                if ctype == "play":
+                    plays[-1] += speak_xml
+            elif item.attrib.get("rend", "") == "speaker":
                 nexupspeaker = True
             elif nexupspeaker and item.text:
-                speak_xml = '\n<speaker>' + escape(item.text) + '</speaker>\n'
+                speak_xml = "\n<speaker>" + escape(item.text) + "</speaker>\n"
                 nexupspeaker = False
 
             else:
                 if item.text:
-                    if ctype == 'chapter':
-                       chapters[-1] += escape(item.text)
-                    if ctype == 'act':
-                       acts[-1] += escape(item.text)
-                    if ctype == 'play':
-                       plays[-1] += escape(item.text)
-
+                    if ctype == "chapter":
+                        chapters[-1] += escape(item.text)
+                    if ctype == "act":
+                        acts[-1] += escape(item.text)
+                    if ctype == "play":
+                        plays[-1] += escape(item.text)
 
     return read_order, speakerlist, alias
